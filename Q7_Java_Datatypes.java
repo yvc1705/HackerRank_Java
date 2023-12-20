1c675cdd03ed4f326149e1e89e5cb8ac89ae34ef
yvc1705 <153513568+yvc1705@users.noreply.github.com>
2023-12-20 23:13:11 +0530

    Add files via upload

--- Q7_Java_Datatypes.java
+++ Q7_Java_Datatypes.java
@@ -31,46 +31,37 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-//public class Solution {
-public class Q7_Java_Datatypes 
-{
-    static String whoCanFitNumber (String numString)
+public class Q7_Java_Datatypes //public class Solution {
     {
-        String answer  = "";
-        try {
+
+    static String whoCanFitTheNumber(String numString)
+    {
+        String answer = "";
+        try{
             long num = Long.parseLong(numString);
-            answer = numString + " can be fitted in: \n";
-            if ((num < Byte.MAX_VALUE) && (num >= Byte.MIN_VALUE))
-            {
-                answer = answer.concat("* byte\n* short\n* int\n* long ");
-            }
-            else if ((num <= Short.MAX_VALUE)&&(num>=Short.MIN_VALUE))
-            {
+            answer = numString + " can be fitted in:\n";
+            if((num<=Byte.MAX_VALUE) && (num>=Byte.MIN_VALUE)){
+                answer = answer.concat("* byte\n* short\n* int\n* long");
+            }else if((num <= Short.MAX_VALUE) && (num >= Short.MIN_VALUE)){
                 answer = answer.concat("* short\n* int\n* long");
+            }else if((num <= Integer.MAX_VALUE) && (num >= Integer.MIN_VALUE)){
+                    answer = answer.concat("* int\n* long");
+            }else{
+                    answer = answer.concat("* long");
             }
-            else if ((num <= Integer.MAX_VALUE) && (num>= Integer.MIN_VALUE))
-            {
-                answer = answer.concat("* int\n* long");
-            }
-            else{
-                answer = answer.concat("* long");
-            }
-        }
-        catch (NumberFormatException e)
-        {
-            answer = numString+ " can't be fitted anywhere.";
+        }catch (NumberFormatException e){
+            answer = numString+" can't be fitted anywhere.";
         }
         return answer;
     }
-    public static void main (String [] args)
-    {
-        Scanner sc = new Scanner (System.in);
-        int numTestCase = sc.nextInt();
-        sc.nextLine();
-        for(int i = 0; i<numTestCase ; i++)
-        {
-            String numString  = sc.nextLine();
-            System.out.println(whoCanFitNumber(numString));
+    public static void main(String[] args) {
+    
+        Scanner scanner = new Scanner(System.in);
+        int numTestCases = scanner.nextInt() ;
+        scanner.nextLine();
+        for(int i=0; i<numTestCases;i++){
+            String numString = scanner.nextLine();
+            System.out.println(whoCanFitTheNumber(numString));
         }
     }
 }
